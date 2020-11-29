6f3351467d55a5412d18b1bdcda41bf6cd8dbabd
Driant Naun <driantn@gmail.com>
2020-11-29 14:25:25 +0100

    create or update entry

--- src/routes/house-conditions.js
+++ src/routes/house-conditions.js
@@ -7,14 +7,28 @@ const router = Router();
 router.post('/house-conditions', async (req, res, next) => {
   const { temperature, humidity } = req.body;
   const timestamp = Math.floor(new Date().getTime() / 1000);
-  console.log(temperature, humidity, timestamp);
   try {
-    const newEntry = fbDatabaseRef.push();
-    newEntry.set({
-      temperature,
-      humidity,
-      timestamp,
-    });
+    // const newEntry = fbDatabaseRef.push();
+    // newEntry.set({
+    //   temperature,
+    //   humidity,
+    //   timestamp,
+    // });
+    const entry = await fbDatabaseRef.once('value');
+    const entryData = entry.val();
+    console.log(entryData);
+    if (!entryData) {
+      const newEntry = fbDatabaseRef.push();
+      newEntry.set({
+        temperature,
+        humidity,
+        timestamp,
+      });
+    } else {
+      const [key] = Object.keys(entryData);
+      console.log(key);
+      fbDatabaseRef.child(key).update({ temperature, humidity, timestamp });
+    }
   } catch (error) {
     console.log('log', error);
   }
